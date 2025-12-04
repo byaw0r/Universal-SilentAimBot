f4911b8d83eaf03fee406c5bbbc4034f28e83a07
byaw0r <rrzm001@gmail.com>
2025-12-05 00:48:41 +0300

    BYW.lua

--- BYW.lua
+++ BYW.lua
@@ -29,7 +29,7 @@ UICorner.Parent = hitboxBtn
 local espConnection
 local originalSizes = {}
 local originalTransparencies = {}
-local hitboxSize = 25
+local hitboxSize = 20
 
 local function getHead(character)
     return character:FindFirstChild("Head")
